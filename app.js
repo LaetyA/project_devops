1d840bf9cc5356bdcb4c04dde96e7cfaa98a7d7d
LaetyA <atelietania8@gmail.com>
2024-01-11 23:37:13 +0000

    ajout

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/new', (req, res) => res.send('heureux de vous voir!'));
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
